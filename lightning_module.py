1553fd2784483eef2924160f5656e65517cf2b0b
DarkAngel007-design <f20251542@pilani.bits-pilani.ac.in>
2026-01-30 22:56:00 +0530

    Update lightning_module.py

--- lightning_module.py
+++ lightning_module.py
@@ -1,6 +1,10 @@
 import pytorch_lightning as pl
 import torch.nn as nn
 import torch
+import numpy as np
+
+from llmwrapper.eval import evaluate_multitask
+
 
 class Tox21LightningModule(pl.LightningModule):
     def __init__(self, model, lr_backbone=1e-5, lr_head=5e-4):
@@ -9,6 +13,8 @@ class Tox21LightningModule(pl.LightningModule):
         self.loss_fn=nn.BCEWithLogitsLoss(reduction="none")
         self.lr_backbone=lr_backbone
         self.lr_head=lr_head
+
+        self.validation_outputs = []
 
     def training_step(self, batch, batch_idx):
         logits=self.model(
@@ -29,10 +35,27 @@ class Tox21LightningModule(pl.LightningModule):
         )
 
         return{
-            "logits": torch.sigmoid(logits).detach(),
-            "labels": batch["labels"],
-            "weights": batch["weights"],
+            "probs": torch.sigmoid(logits).detach().cpu().numpy(),
+            "labels": batch["labels"].cpu().numpy(),
+            "weights": batch["weights"].cpu().numpy(),
         }
+    
+    def on_validation_batch_end(self, outputs, batch, batch_idx, dataloader_idx = 0):
+        self.validation_outputs.append(outputs)
+
+    def on_validation_epoch_end(self):
+        y_pred = np.concatenate([o["probs"] for o in self.validation_outputs], axis=0)
+        y_true = np.concatenate([o["labels"] for o in self.validation_outputs], axis=0)
+        w = np.concatenate([o["weights"] for o in self.validation_outputs], axis=0)
+
+        metrics = evaluate_multitask(y_true, y_pred, w)
+
+        if metrics["roc_auc"] is not None:
+            self.log("val_roc_auc", metrics["roc_auc"], prog_bar=True)
+            self.log("val_pr_auc", metrics["pr_auc"], prog_bar=True)
+            self.log("val_n_tasks", metrics["n_valid_tasks"])
+
+        self.validation_outputs.clear()
     
     def configure_optimizers(self):
         return torch.optim.AdamW(
