836e92f0156dfc3a8ec514d8dc61aa0e1ec0b12f
DarkAngel007-design <f20251542@pilani.bits-pilani.ac.in>
2026-02-01 12:02:40 +0530

    Update dataset.py

--- pretraining/dataset.py
+++ pretraining/dataset.py
@@ -1,11 +1,19 @@
 from torch.utils.data import Dataset
 
 class SmilesMLMDataset(Dataset):
-    def __init__(self, smiles_list):
+    def __init__(self, smiles_list, tokenizer, max_length=128):
         self.smiles = smiles_list
+        self.tokenizer = tokenizer
+        self.max_length = max_length
 
     def __len__(self):
         return len(self.smiles)
-    
+
     def __getitem__(self, idx):
-        return self.smiles[idx]
+        return self.tokenizer(
+            self.smiles[idx],
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_length,
+            return_tensors="pt",
+        )["input_ids"].squeeze(0)
